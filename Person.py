6a0d6399a52d4cf3f3aa93664b55bdaf388c1e3d
Luca <luca.porru@hotmail.fi>
2022-03-02 16:39:33 +0200

    New python file made

---
+++ Person.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def return_name(self):
+        return self.name
+
+    def set_name(self, x: str):
+        self.name = x
+
+    def return_age(self):
+        return self.age
+
+    def set_age(self, y: int):
+        self.age = y
+    
+    def __str__(self):
+        return f"Hi, I'm {self.name} and I am ({self.age}) years old"
+
+    
+testi = Person("Timo", 35)
+
+print(testi.name)
+print(testi.age)
+    
